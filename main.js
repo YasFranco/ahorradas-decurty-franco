57a1a6d6adedd7fc56415ff50698c98f42462209
Natasha <natashadecurty@gmail.com>
2025-02-23 17:43:05 -0600

    funcionalidad del btnMenu con errores

--- main.js
+++ main.js
@@ -2,3 +2,12 @@ const $ = (elem) => document.querySelector(elem);
 const $$ = (elem) => document.querySelectorAll(elem);
 
 const $btnMenu = $('#button-menu');
+const $btnMenuClose = $('#button-menu-close');
+const $navMenuMobile = $('#div-mobile-nav-list');
+
+$btnMenu.addEventListener('click', () => {
+    $btnMenu.classList.add('hidden');
+    $btnMenuClose.classList.remove('hidden');
+
+});
+
