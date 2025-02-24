637b67d46551d54d5ab26c95451a40f14c310fca
yasminfranco <yasfranco98@gmail.com>
2025-02-23 21:05:22 -0300

    Solucionando problema de icono hamburguesa

--- main.js
+++ main.js
@@ -6,8 +6,15 @@ const $btnMenuClose = $('#button-menu-close');
 const $navMenuMobile = $('#div-mobile-nav-list');
 
 $btnMenu.addEventListener('click', () => {
-    $btnMenu.classList.add('hidden');
-    $btnMenuClose.classList.remove('hidden');
+    $btnMenu.classList.add("hidden");
+    $btnMenuClose.classList.remove("hidden")
+    $navMenuMobile.classList.remove("hidden")
 
 });
+
+$btnMenuClose.addEventListener('click', () => {
+    $btnMenu.classList.remove("hidden");
+    $btnMenuClose.classList.add("hidden")
+    $navMenuMobile.classList.add("hidden")
+}) 
 
